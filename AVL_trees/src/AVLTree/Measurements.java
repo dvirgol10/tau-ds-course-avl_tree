efbe6629bd19158a3c50c6c061f67241c50c5ab8
Itay Peleg <itayo252@gmail.com>
2021-05-13 20:42:06 +0300

    Finished writing tests

--- AVL_trees/src/AVLTree/Measurements.java
+++ AVL_trees/src/AVLTree/Measurements.java
@@ -1,5 +1,8 @@
 package AVLTree;
-import java.util.Date;
+
+import AVLTree.AVLTree.BSTree;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -7,7 +10,6 @@ public class Measurements {
 
     private static int[] randomIntArray(int size){
         Random random = new Random();
-
         int[] intArr = new int[size];
         for (int i = 0; i < intArr.length; i++) {
             intArr[i] = random.nextInt();
@@ -22,14 +24,17 @@ public class Measurements {
     }
 
     private static void printAverageTime(long time, int counter, String funcName) {
-        System.out.println(String.format("Average %d executions time of %s in milliseconds: %t", counter, funcName, time));
+        System.out.println(String.format("Average %d executions time of %s in milliseconds:  %f", counter, funcName, time / 1000000.0));
     }
 
     private static void comparePrefixXor(){
-        for (int i = 1; i <= 5; i++){
-            AVLTree avlTree = new AVLTree();
+        AVLTree avlTree;
+        int[] keysArray;
 
-            int[] keysArray = randomIntArray(500 * i);
+        for (int i = 1; i <= 5; i++){
+            avlTree = new AVLTree();
+
+            keysArray  = randomIntArray(500 * i);
 
             insertMany(avlTree, keysArray);
 
@@ -37,16 +42,18 @@ public class Measurements {
             int counter = 0;
             long timePrefixXor = 0;
             long timeSuccPrefixXor = 0;
+            long startTime;
+            long endTime;
 
             for (int k : keysArray){
-                long startTime = new Date().getTime();
+                startTime = System.nanoTime();
                 avlTree.prefixXor(k);
-                long endTime = new Date().getTime();
+                endTime = System.nanoTime();
                 timePrefixXor += endTime - startTime;
 
-                startTime = new Date().getTime();
+                startTime = System.nanoTime();
                 avlTree.succPrefixXor(k);
-                endTime = new Date().getTime();
+                endTime = System.nanoTime();
                 timeSuccPrefixXor += endTime - startTime;
 
                 counter += 1;
@@ -60,12 +67,119 @@ public class Measurements {
             printAverageTime(timePrefixXor, counter, "prefixXor");
             printAverageTime(timeSuccPrefixXor, counter, "succPrefixXor");
         }
+    }
+
+    private static int[] arithmeticProgressionArray(int size){
+        int[] intArr = new int[size];
+        for (int i = 0; i < intArr.length; i++) {
+            intArr[i] = i + 1;
+        }
+        return intArr;
+    }
+
+    /*
+    public void inOrderArray(int[] arithmeticProgressionArr, int offset, int[] arr) {
+        if (node.isRealNode()) {
+            inOrder(node.getLeft(), offset, arr);
+            arr[offset + node.getLeft().getSize()] = node; // assigns the node in the right position in the in-order array
+            inOrder(node.getRight(), offset + node.getLeft().getSize() + 1, arr);
+        }
+    }
+    */
+
+    /*
+    private static int[] balancedSeriesArray(int size){
+        int[] intArr = new int[size];
+        for (int i = 0; i < intArr.length; i++) {
+            intArr[i] = i + 1;
+        }
+        return intArr;
+    }
+    */
+    private static int[] balancedSeriesArray(int i) {
+        return Arrays.copyOfRange(envBalancedSeriesArray(minimalPowerOf2(i)),0, i);
+    }
+
+    private static int minimalPowerOf2(int i) {
+        return powOf2(log2(i)) == i ? i : powOf2(log2(i) + 1);
+    }
+
+    private static int[] envBalancedSeriesArray(int size){
+        int[] ret = new int[size];
+        int startLevel;
+        for (int i = 0; i < log2(size); i++) {
+            startLevel = powOf2(i);
+            for (int j = 0; j < startLevel; j++) {
+                ret[startLevel - 1 + j] = size * (1+2*j) / (startLevel*2);
+            }
+        }
+        ret[size-1] = size;
+        return ret;
+    }
+
+    private static int powOf2(int i2) {
+        return (int) Math.pow(2, i2);
+    }
+
+    private static int log2(int i) {
+        return (int) (Math.log(i) / Math.log(2));
+    }
 
 
+
+    private static long[] insertTimeCalc(int[] keysArray) {
+        AVLTree avlTree = new AVLTree();
+        BSTree bsTree = new BSTree();
+
+        long timeInsertAVL = 0;
+        long timeInsertBS = 0;
+
+        long startTime;
+        long endTime;
+
+        for (int k : keysArray){
+            startTime = System.nanoTime();
+            avlTree.insert(k, false);
+            endTime = System.nanoTime();
+            timeInsertAVL += endTime - startTime;
+
+            startTime = System.nanoTime();
+            bsTree.insert(k, false);
+            endTime = System.nanoTime();
+            timeInsertBS += endTime - startTime;
+        }
+
+        return new long[]{timeInsertAVL, timeInsertBS};
+    }
+
+    private static void compareInsert(){
+        long[] times;
+        int size;
+        int[] keysArray;
+
+        for (int i = 1; i <= 5; i++){
+            size = 1000 * i;
+
+            keysArray = arithmeticProgressionArray(size);
+            times = insertTimeCalc(keysArray);
+            printAverageTime(times[0], size, "AVL(arithmetic progression)");
+            printAverageTime(times[1], size, "BS(arithmetic progression)");
+
+            keysArray = balancedSeriesArray(size);
+            times = insertTimeCalc(keysArray);
+            printAverageTime(times[0], size, "AVL(balanced series)");
+            printAverageTime(times[1], size, "BS(balanced series)");
+
+            keysArray = randomIntArray(size);
+            times = insertTimeCalc(keysArray);
+            printAverageTime(times[0], size, "AVL(random)");
+            printAverageTime(times[1], size, "BS(random)");
+        }
     }
 
     public static void main(String[] args) {
         comparePrefixXor();
+        compareInsert();
     }
 
 }
